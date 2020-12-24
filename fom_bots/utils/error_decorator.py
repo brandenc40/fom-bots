5354d7b7a9c43a5c4c71d06e6193f5cb01b29680
Branden Colen <brandenc@uber.com>
2020-12-24 11:39:01 -0600

    added ctx var to f()

--- fom_bots/utils/error_decorator.py
+++ fom_bots/utils/error_decorator.py
@@ -9,7 +9,7 @@ def handle_exceptions(f):
     @wraps(f)
     def wrapper(ctx: Context):
         try:
-            f()
+            f(ctx)
         except Exception as e:
             logger.error(str(e), exc_info=e)
             ctx.bot.post_message("Shits broken... The function `{}` threw this error: {}".format(f.__name__, str(e)))
