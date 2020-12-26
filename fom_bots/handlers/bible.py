572b5de68d5174989c7a1672672c19fe3b10be60
Branden <brandencolen@gmail.com>
2020-12-26 16:20:23 -0600

    added comebacks

--- fom_bots/handlers/bible.py
+++ fom_bots/handlers/bible.py
@@ -10,7 +10,14 @@ NAME_IN_VANE_RESPONSES = [
     "Watch your tongue, you heathen.",
     "May the Holy Name of God be blessed. Begone satan.",
     "Hey friend, don’t take the Lord’s Name in vain! It ain’t good for you!",
-    "Blessed be thy name."
+    "Blessed be thy name.",
+    "I notice you talk about God/Jesus a lot. I'm curious to know what you think about God, exactly? I'd love to "
+    "discuss it!",
+    "You're going to Hell.",
+    "The Bible says don't say that.",
+    "You must not misuse the name of the LORD your God. The LORD will not let you go unpunished if you misuse his "
+    "name. - Deuteronomy 5:10-11",
+    "Do not bring shame on the name of your God by using it to swear falsely. I am the LORD. - Leviticus 19:12"
 ]
 
 
