22bf110760ed9d617535a1e784eeb64226b3bae7
Branden <brandencolen@gmail.com>
2020-12-26 16:07:28 -0600

    one of top 5 results

--- fom_bots/gateways/pornhub.py
+++ fom_bots/gateways/pornhub.py
@@ -9,6 +9,7 @@ def search_videos(search_string: str) -> str:
         params={"search": search_string}
     )
     res.raise_for_status()
-    videos = res.json()['videos']
-    video = videos[randint(0, len(videos) - 1)]
+    json = res.json()
+    videos = json['videos']
+    video = videos[randint(0, min(len(videos) - 1, 5))]
     return video['url']
